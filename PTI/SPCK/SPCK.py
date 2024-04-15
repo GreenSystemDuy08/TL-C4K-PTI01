f3a912e3d7a8108e25ac387a32fd7be137941bfa
Nguyễn Hữu Duy <duynguoibac@gmail.com>
2024-04-15 22:10:00 +0700

    feat Update SPCK
    Update App Note for WOW! to
    Final April Test Release - 15/04/2024

--- PTI/SPCK/SPCK.py
+++ PTI/SPCK/SPCK.py
@@ -3,183 +3,71 @@ from tkinter import *
 from PyQt6.QtWidgets import QMainWindow, QApplication, QMessageBox , QWidget, QFileDialog
 from PyQt6 import uic
 
-class MainNotePage(QMainWindow, QWidget):
+class SetupPage(QMainWindow, QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("GUI/mainnote.ui", self)
-        self.bt_edit1.clicked.connect(self.showEditNote1)
-        self.bt_edit2.clicked.connect(self.showEditNote2)
-        self.bt_add.clicked.connect(self.showCaution)
-        self.bt_quit.clicked.connect(self.Close)
-        self.bt_save.clicked.connect(self.showSignIn)
-        self.bt_apply.clicked.connect(self.checkName)
-    def showSignIn(self):
-        SignIn.show()
-        self.close()
-    def Close(self):
-        self.close()
-    def showCaution(self):
-        msg_box1.setText("The notes page has been added successfully!")
-        msg_box1.exec()
-        return
-    def showEditNote1(self):
-        EditNote1.show()
-    def showEditNote2(self):
-        EditNote2.show()
-    def checkName(self):
-        name = self.le_name.text()
-
-        if not name:
-            msg_box.setText("Please enter a title name before clicking Apply!")
-            msg_box.exec()
-            return
-        else:
-            msg_box1.setText("Apply title name successfully!")
-            msg_box1.exec()
+        uic.loadUi("GUI/setup.ui", self)
+        self.bt_next.clicked.connect(self.check)
+    def check(self):
+        if self.rb_portable.isChecked():
+            Setup2_2.show()
+            self.close()
             return
         
-class EditNote1Page(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("GUI/editNote1.ui", self)
-        self.bt_save.clicked.connect(self.Close)
-    def Close(self):
-        self.close()
+        if self.rb_install.isChecked():
+            Setup1.show()
+            self.close()
+            return
+        
+        elif not self.rb_portable.isChecked():
+            msg_box.setText("Please choose the installation method!")
+            msg_box.exec()
+            return
+        
+        elif not self.rb_install.isChecked():
+            msg_box.setText("Please choose the installation method!")
+            msg_box.exec()
+            return
 
-class EditNote2Page(QMainWindow, QWidget):
+class Setup2_2Page(QMainWindow, QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("GUI/editNote2.ui", self)
-        self.bt_save.clicked.connect(self.Close)
-    def Close(self):
+        uic.loadUi("GUI/setup2.ui", self)
+        self.bt_next.clicked.connect(self.showSetup3)
+        self.bt_back.clicked.connect(self.back)
+    def back(self):
+        Setup.show()
         self.close()
+    def showSetup3(self):
+        if not self.cb_language.currentIndex():
+            msg_box.setText("Please check and select the program language!")
+            msg_box.exec()
+            return
 
-class AdminPage(QMainWindow, QWidget):
+        elif not self.cb_edition.currentIndex():
+            msg_box.setText("Please check and select this app edtion!")
+            msg_box.exec()
+            return
+
+        if not self.chb_accept.isChecked():
+            msg_box.setText("Please agree to the terms of this Application!")
+            msg_box.exec()
+        if self.chb_accept.isChecked():
+            Finish.show()
+            self.close()
+
+class FinishPage(QMainWindow, QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("GUI/AdminPage.ui", self)
-        self.bt_add.clicked.connect(self.showCaution)
-        self.bt_save.clicked.connect(self.showSignIn)
-        self.bt_exit.clicked.connect(self.showSignIn)
-        self.bt_tool1.clicked.connect(self.showTool1)
-        self.bt_tool2.clicked.connect(self.showTool2)
-        self.bt_edit1.clicked.connect(self.showEditNote1)
-        self.bt_edit2.clicked.connect(self.showEditNote2)
-        self.bt_setting.clicked.connect(self.showSetting)
-        self.bt_apply.clicked.connect(self.checkName)
-    def showCaution(self):
-        msg_box1.setText("The notes page has been added successfully!")
-        msg_box1.exec()
-        return
-    def showTool1(self):
-        Tool1.show()
-        self.close()
-    def showTool2(self):
-        Tool2.show()
+        uic.loadUi("GUI/Finish.ui", self)
+        self.bt_finish.clicked.connect(self.showSignIn)
+        self.bt_back.clicked.connect(self.back)
+    def back(self):
+        Setup2_2.show()
         self.close()
     def showSignIn(self):
         SignIn.show()
         self.close()
-    def showEditNote1(self):
-        EditNote1.show()
-    def showEditNote2(self):
-        EditNote2.show()
-    def showSetting(self):
-        Setting.show()
-        self.close()
-    def checkName(self):
-        name = self.le_name.text()
-
-        if not name:
-            msg_box.setText("Please enter a title name before clicking Apply!")
-            msg_box.exec()
-            return
-        else:
-            msg_box1.setText("Apply title name successfully!")
-            msg_box1.exec()
-            return
-
-class SettingPage(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("GUI/setting.ui", self)
-        self.bt_save.clicked.connect(self.back)
-        self.bt_reset.clicked.connect(self.showCautionReset)
-        self.bt_delete.clicked.connect(self.showCautionDelete)
-    def back(self):
-        if self.chb_enable.isChecked():
-            msg_box.setText("ERROR: This feature is not currently available or being tested!\nThis Program will not enable this feature")
-            msg_box.exec()
-        
-        if self.chb_detail.isChecked():
-            msg_box.setText("ERROR: This feature is not currently available or being tested!\nThis Program will not enable this feature")
-            msg_box.exec()
-        
-        AdminTool.show()
-        self.close()
-    def showCautionDelete(self):
-        msg_box1.setText("All of the notes page has been successfully removed!")
-        msg_box1.exec()
-        return
-    def showCautionReset(self):
-        msg_box1.setText("Everything has been reset to factory defaults!")
-        msg_box1.exec()
-        return
-
-class Tool1Page(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("GUI/AdminTool - note1.ui", self)
-        self.bt_back.clicked.connect(self.back)
-        self.bt_remove.clicked.connect(self.Caution)
-        self.bt_detail.clicked.connect(self.showNoteDetail)
-    def back(self):
-        AdminTool.show()
-        self.close()
-    def Caution(self):
-        msg_box1.setText("The notes page has been successfully removed!")
-        msg_box1.exec()
-        return
-    def showNoteDetail(self):
-        NoteDetail1.show()
-        self.close()
-
-class Tool2Page(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("GUI/AdminTool - note2.ui", self)
-        self.bt_back.clicked.connect(self.back)
-        self.bt_remove.clicked.connect(self.Caution)
-        self.bt_detail.clicked.connect(self.showNoteDetail)
-    def Caution(self):
-        msg_box1.setText("The notes page has been successfully removed!")
-        msg_box1.exec()
-        return
-    def back(self):
-        AdminTool.show()
-        self.close()
-    def showNoteDetail(self):
-        NoteDetail2.show()
-        self.close()
-
-class DetailPage1(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("GUI/noteDetail1.ui", self)
-        self.bt_close.clicked.connect(self.back)
-    def back(self):
-        Tool1.show()
-        self.close()
-
-class DetailPage2(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("GUI/noteDetail2.ui", self)
-        self.bt_close.clicked.connect(self.back)
-    def back(self):
-        Tool2.show()
-        self.close()
-
 
 class Setup1Page(QMainWindow, QWidget):
     def __init__(self):
@@ -187,15 +75,15 @@ class Setup1Page(QMainWindow, QWidget):
         uic.loadUi("GUI/setup1.ui", self)
         self.bt_next.clicked.connect(self.showSetup2)
         self.bt_browse.clicked.connect(self.browsecsv)
-        # self.bt_browse.clicked.connect(self.showBrowse)
+        self.bt_back.clicked.connect(self.back)
+    def back(self):
+        Setup.show()
+        self.close()
     def showSetup2(self):
         Setup2.show()
         self.close()
-    # def showBrowse(self):
-    #     msg_box.setText("ERROR! Browse not found!")
-    #     msg_box.exec()
     def browsecsv(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
+        folder_path = QFileDialog.getExistingDirectory(self, "Select the destination path")
         if folder_path:
             self.lineEdit.setText(folder_path)
         else:
@@ -206,6 +94,10 @@ class Setup2Page(QMainWindow, QWidget):
         super().__init__()
         uic.loadUi("GUI/setup2.ui", self)
         self.bt_next.clicked.connect(self.showSetup3)
+        self.bt_back.clicked.connect(self.back)
+    def back(self):
+        Setup1.show()
+        self.close()
     def showSetup3(self):
         if not self.cb_language.currentIndex():
             msg_box.setText("Please check and select language to install!")
@@ -229,6 +121,10 @@ class Setup3Page(QMainWindow, QWidget):
         super().__init__()
         uic.loadUi("GUI/setup3.ui", self)
         self.bt_install.clicked.connect(self.showSetupFinish)
+        self.bt_back.clicked.connect(self.back)
+    def back(self):
+        Setup2.show()
+        self.close()
     def showSetupFinish(self):
         SetupFinish.show()
         self.close()
@@ -368,14 +264,210 @@ class SignUpPage(QMainWindow, QWidget):
             msg_box.exec()
             return
 
+class MainNotePage(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/mainnote.ui", self)
+        self.bt_edit1.clicked.connect(self.showEditNote1)
+        self.bt_edit2.clicked.connect(self.showEditNote2)
+        self.bt_add.clicked.connect(self.showCaution)
+        self.bt_quit.clicked.connect(self.Close)
+        self.bt_save.clicked.connect(self.showSignIn)
+        self.bt_apply.clicked.connect(self.checkName)
+        self.bt_font.clicked.connect(self.showFont)
+    def showFont(self):
+        Font.show()
+        self.close()
+    def showSignIn(self):
+        SignIn.show()
+        self.close()
+    def Close(self):
+        self.close()
+    def showCaution(self):
+        msg_box1.setText("The notes page has been added successfully!")
+        msg_box1.exec()
+        return
+    def showEditNote1(self):
+        EditNote1.show()
+    def showEditNote2(self):
+        EditNote2.show()
+    def checkName(self):
+        name = self.le_name.text()
+
+        if not name:
+            msg_box.setText("Please enter a title name before clicking Apply!")
+            msg_box.exec()
+            return
+        else:
+            msg_box1.setText("Apply title name successfully!")
+            msg_box1.exec()
+            return
+        
+class FontPage(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/font.ui", self)
+        self.bt_ok.clicked.connect(self.back)
+    def back(self):
+        MainNote.show()
+        self.close()
+        
+class EditNote1Page(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/editNote1.ui", self)
+        self.bt_save.clicked.connect(self.Close)
+    def Close(self):
+        self.close()
+
+class EditNote2Page(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/editNote2.ui", self)
+        self.bt_save.clicked.connect(self.Close)
+    def Close(self):
+        self.close()
+
+class AdminPage(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/AdminPage.ui", self)
+        self.bt_add.clicked.connect(self.showCaution)
+        self.bt_save.clicked.connect(self.showSignIn)
+        self.bt_exit.clicked.connect(self.showSignIn)
+        self.bt_tool1.clicked.connect(self.showTool1)
+        self.bt_tool2.clicked.connect(self.showTool2)
+        self.bt_edit1.clicked.connect(self.showEditNote1)
+        self.bt_edit2.clicked.connect(self.showEditNote2)
+        self.bt_setting.clicked.connect(self.showSetting)
+        self.bt_apply.clicked.connect(self.checkName)
+    def showCaution(self):
+        msg_box1.setText("The notes page has been added successfully!")
+        msg_box1.exec()
+        return
+    def showTool1(self):
+        Tool1.show()
+        self.close()
+    def showTool2(self):
+        Tool2.show()
+        self.close()
+    def showSignIn(self):
+        SignIn.show()
+        self.close()
+    def showEditNote1(self):
+        EditNote1.show()
+    def showEditNote2(self):
+        EditNote2.show()
+    def showSetting(self):
+        Setting.show()
+        self.close()
+    def checkName(self):
+        name = self.le_name.text()
+
+        if not name:
+            msg_box.setText("Please enter a title name before clicking Apply!")
+            msg_box.exec()
+            return
+        else:
+            msg_box1.setText("Apply title name successfully!")
+            msg_box1.exec()
+            return
+
+class SettingPage(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/setting.ui", self)
+        self.bt_save.clicked.connect(self.back)
+        self.bt_reset.clicked.connect(self.showCautionReset)
+        self.bt_delete.clicked.connect(self.showCautionDelete)
+    def back(self):
+        if self.chb_enable.isChecked():
+            msg_box.setText("ERROR: This feature is not currently available or being tested!\nThis Program will not enable this feature")
+            msg_box.exec()
+            return
+        
+        if self.chb_detail.isChecked():
+            msg_box.setText("ERROR: This feature is not currently available or being tested!\nThis Program will not enable this feature")
+            msg_box.exec()
+            return
+        
+        AdminTool.show()
+        self.close()
+    def showCautionDelete(self):
+        msg_box1.setText("All of the notes page has been successfully removed!")
+        msg_box1.exec()
+        return
+    def showCautionReset(self):
+        msg_box1.setText("Everything has been reset to factory defaults!")
+        msg_box1.exec()
+        return
+
+class Tool1Page(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/AdminTool - note1.ui", self)
+        self.bt_back.clicked.connect(self.back)
+        self.bt_remove.clicked.connect(self.Caution)
+        self.bt_detail.clicked.connect(self.showNoteDetail)
+    def back(self):
+        AdminTool.show()
+        self.close()
+    def Caution(self):
+        msg_box1.setText("The notes page has been successfully removed!")
+        msg_box1.exec()
+        return
+    def showNoteDetail(self):
+        NoteDetail1.show()
+        self.close()
+
+class Tool2Page(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/AdminTool - note2.ui", self)
+        self.bt_back.clicked.connect(self.back)
+        self.bt_remove.clicked.connect(self.Caution)
+        self.bt_detail.clicked.connect(self.showNoteDetail)
+    def Caution(self):
+        msg_box1.setText("The notes page has been successfully removed!")
+        msg_box1.exec()
+        return
+    def back(self):
+        AdminTool.show()
+        self.close()
+    def showNoteDetail(self):
+        NoteDetail2.show()
+        self.close()
+
+class DetailPage1(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/noteDetail1.ui", self)
+        self.bt_close.clicked.connect(self.back)
+    def back(self):
+        Tool1.show()
+        self.close()
+
+class DetailPage2(QMainWindow, QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("GUI/noteDetail2.ui", self)
+        self.bt_close.clicked.connect(self.back)
+    def back(self):
+        Tool2.show()
+        self.close()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    MainPage = Setup1Page()
-    Setup1 = Setup1Page()
+    MainPage = SetupPage()
+    Setup = SetupPage()
     MainPage.show()
+    Finish = FinishPage()
+    Setup1 = Setup1Page()
     AdminTool = AdminPage()
     MainNote = MainNotePage()
+    Font = FontPage()
+    Setup2_2 = Setup2_2Page()
     Setup2 = Setup2Page()
     Setup3 = Setup3Page()
     SetupFinish = SetupFinishPage()
